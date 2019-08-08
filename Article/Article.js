66647336d4b74186ce4e0638b262e93046789c68
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-08-08 10:32:25 -0400

    completed article.js file

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,46 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+const articles = document.querySelector('.articles')
+
+data.forEach(data => {
+  articles.appendChild(createArticle(data.title, data.data, data.para1, data.para2, data.para3))
+})
+
+function createArticle(title, date, firstParagraph, secondParagraph, thirdParagraph) {
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const para1 = document.createElement('p');
+  const para2 = document.createElement('p');
+  const para3 = document.createElement('p');
+  const para4 = document.createElement('p');
+  const expandButton = document.createElement('span');
+
+  article.appendChild(articleTitle);
+  article.appendChild(articleDate);
+  article.appendChild(para1);
+  article.appendChild(para2);
+  article.appendChild(para3);
+  article.appendChild(expandButton);
+
+  //set class names
+  article.classList.add('article');
+  articleDate.classList.add('date');
+  expandButton.classList.add('expandButton');
+
+  //Set text content
+  expandButton.textContent = 'expand'
+  articleTitle.textContent = title
+  articleDate.textContent = date
+  para1.textContent = firstParagraph
+  para2.textContent = secondParagraph
+  para3.textContent = thirdParagraph
+
+  //Button events
+  expandButton.addEventListener
+  .addEventListener('click', event => {
+    article.classList.toggle('article-open');
+  })
+  return article
+}
